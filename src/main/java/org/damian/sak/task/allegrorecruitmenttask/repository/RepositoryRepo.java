d1b1a17903381e6620a74d7d5c09d74064af288a
damianSak <damian.sak88@gmail.com>
2022-01-08 13:01:26 +0100

    added thrown exceptions to all method signatures

--- src/main/java/org/damian/sak/task/allegrorecruitmenttask/repository/RepositoryRepo.java
+++ src/main/java/org/damian/sak/task/allegrorecruitmenttask/repository/RepositoryRepo.java
@@ -1,15 +1,22 @@
 package org.damian.sak.task.allegrorecruitmenttask.repository;
 
+import org.damian.sak.task.allegrorecruitmenttask.exception.custom_exception.RepositoriesNotFoundException;
+import org.damian.sak.task.allegrorecruitmenttask.exception.custom_exception.UserNotFoundException;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Repository
 public interface RepositoryRepo {
 
-    Map<String, Integer> getAllReposWithRatings(String userName);
+    Map<String, Integer> getAllReposWithRatings(String userName) throws UserNotFoundException,
+            RepositoriesNotFoundException, IOException;
 
-    int getTotalReposRating(String userName);
+    int getTotalReposRating(String userName) throws UserNotFoundException,
+            RepositoriesNotFoundException, IOException;
 
-    Map<String, Integer> getProgrammingLanguageRatingBySize(String userName);
+    Map<String, Integer> getProgrammingLanguageRatingBySize(String userName) throws UserNotFoundException,
+            RepositoriesNotFoundException, IOException;
+
 }
