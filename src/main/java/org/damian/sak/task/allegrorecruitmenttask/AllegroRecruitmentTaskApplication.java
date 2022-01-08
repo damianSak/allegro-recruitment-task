681f964ac106ac515630c8431367085ff16bc604
damianSak <damian.sak88@gmail.com>
2022-01-08 11:16:54 +0100

    initial version

---
+++ src/main/java/org/damian/sak/task/allegrorecruitmenttask/AllegroRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package org.damian.sak.task.allegrorecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroRecruitmentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/damian/sak/task/allegrorecruitmenttask/AllegroRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.damian.sak.task.allegrorecruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroRecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
