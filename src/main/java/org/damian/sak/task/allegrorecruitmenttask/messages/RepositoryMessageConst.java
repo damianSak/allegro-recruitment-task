f4db815b8af9811d45a806a0512da154d91ad31e
damianSak <damian.sak88@gmail.com>
2022-01-08 19:21:10 +0100

    initial version

---
+++ src/main/java/org/damian/sak/task/allegrorecruitmenttask/messages/RepositoryMessageConst.java
@@ -0,0 +1,8 @@
+package org.damian.sak.task.allegrorecruitmenttask.messages;
+
+public class RepositoryMessageConst {
+
+    public static final String USER_NOT_FOUND_EX = "github.user.not.found";
+    public static final String REPOS_NOT_FOUND_EX = "github.user.without.public.repos";
+}
+
