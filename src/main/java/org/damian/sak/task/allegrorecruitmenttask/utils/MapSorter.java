f3719f55f4a2df8f3b179bd5a2a07a6da6f7ea35
damianSak <damian.sak88@gmail.com>
2022-01-09 12:50:10 +0100

    added logger and code refactoring

--- src/main/java/org/damian/sak/task/allegrorecruitmenttask/utils/MapSorter.java
+++ src/main/java/org/damian/sak/task/allegrorecruitmenttask/utils/MapSorter.java
@@ -1,19 +1,25 @@
 package org.damian.sak.task.allegrorecruitmenttask.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapSorter {
 
+    private static final Logger logger = LoggerFactory.getLogger(MapSorter.class);
+
     private MapSorter() {
     }
 
     /**
-     * This method is sorting map pair's of elements according
+     * This method is sorting pair's of map elements according
      * to it's values in descending order.
-     * @param mapToSort  unsorted map
-     * @return           map sorted according to values inside
+     *
+     * @param mapToSort unsorted map
+     * @return map sorted according to values inside
      */
 
     public static Map<String, Integer> sortMapByValuesDescending(Map<String, Integer> mapToSort) {
@@ -23,7 +29,7 @@ public class MapSorter {
         mapToSort.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
-
+        logger.info("Returning map sorted in descending order by it's values");
         return reverseSortedMap;
     }
 
