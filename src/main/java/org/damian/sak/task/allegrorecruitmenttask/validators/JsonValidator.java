5b2b3c29fc938f68ecdfa74b6b5ce644db3fae4a
damianSak <damian.sak88@gmail.com>
2022-01-09 13:03:02 +0100

    added logger

--- src/main/java/org/damian/sak/task/allegrorecruitmenttask/validators/JsonValidator.java
+++ src/main/java/org/damian/sak/task/allegrorecruitmenttask/validators/JsonValidator.java
@@ -1,17 +1,24 @@
 package org.damian.sak.task.allegrorecruitmenttask.validators;
 
+import org.damian.sak.task.allegrorecruitmenttask.utils.MapSorter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JsonValidator {
 
+    private static final Logger logger = LoggerFactory.getLogger(MapSorter.class);
+
     /**
      * Validate if JSON is populated with data.
+     *
      * @param jsonString JSON to validate
-     * @return           true if JSON have information inside or false if JSON don't have any information inside
+     * @return true if JSON have information inside or false if JSON don't have any information inside
      */
 
     public boolean isJsonEmpty(String jsonString){
+        logger.info("Validating response from GitHub server");
         return jsonString.equalsIgnoreCase("[]");
     }
 }
